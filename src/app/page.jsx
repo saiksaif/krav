5cb5f5e462b19b8ab42be9e031522da8e684cb9a
Saif Ali <116622210+saiksaif@users.noreply.github.com>
2024-02-09 11:56:58 +0500

    Minor additions

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,31 @@
 "use client"
-import { useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import Mysection from "../components/Mysection";
 import Image from "next/image";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+import Section1 from "../components/landing/section1";
 
 
 export default function Home() {
+  const [showComponent, setShowComponent] = useState(false);
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setShowComponent(true);
+    }, 5000);
+    // Clear the timeout if the component is unmounted
+    return () => clearTimeout(timeoutId);
+  }, []);
+
+
+  useEffect(() => {
+    AOS.init({
+      duration: 800,
+      once: false,
+    })
+  }, [])
+
   const section1 = useRef();
   const section2 = useRef();
   const section3 = useRef();
@@ -16,13 +37,15 @@ export default function Home() {
     <main className="overflow-hidden">
       <div className="w-[100vw] h-[80px] fixed text-center grid grid-cols-3">
         <div className=""></div>
-        <div className="flex justify-center">
-          <Image src="/image00007.png" width="130" height="50" className="mt-2" />
+        <div className="flex justify-center navBarLogoBox">
+          <Image src="/image00007.png" width="800" height="600" className="mt-2 navBarLogo" />
         </div>
         <div className="text-right px-6 items-center flex justify-end">
+        {showComponent && 
           <button className="oddFont hover:italic text-3xl font-light">
             MENU
           </button>
+        }
         </div>
       </div>
       <div className="container2 w-full h-[100vh] overflow-hidden">
@@ -31,14 +54,7 @@ export default function Home() {
             <Mysection
               headline={`Lorem Ipsum Dolor Sit Amet`}
             >
-              <div className="text-[200px] flex flex-col items-center justify-center h-full">
-                <div className="letterSpacingLess">
-                  OWN YOUR CULTURE
-                </div>
-                <div className="letterSpacingLess">
-                  TELL YOUR STORIES
-                </div>
-              </div>
+              {showComponent && <Section1 />}
             </Mysection>
           </div>
           <div ref={section2}>
@@ -46,7 +62,7 @@ export default function Home() {
               headline={`Lorem Ipsum Dolor Sit Amet`}
             >
               <div className="w-full h-full flex justify-center items-center">
-                <video src="/Karv-UIUX.mp4" autoPlay></video>
+                <video src="/Karv-UIUX.mp4" autoPlay muted playsInline controlsList="nodownload nofullscreen noremoteplayback"></video>
               </div>
             </Mysection>
           </div>
---
+++ src/components/landing/section1.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import {motion} from 'framer-motion'
+
+const Section1 = () => {
+    useEffect(() => {
+      AOS.init({
+           duration: 800,
+           once: false,
+         })
+    }, [])
+
+    return (
+        // <div>
+            <div className="text-[200px] flex flex-col items-center justify-center h-full">
+                <div className="letterSpacingLess" data-aos="fade-right" data-aos-once="false">
+                    OWN YOUR CULTURE
+                </div>
+                <div className="letterSpacingLess" data-aos="fade-left">
+                    TELL YOUR STORIES
+                </div>
+            </div>
+        // </div>
+    )
+}
+
+export default Section1
